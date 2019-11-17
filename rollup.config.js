4e6fbe51fb0156109cc6ad9d91a7fa8e655e9fa0
awehook <awehook@163.com>
2019-11-17 21:40:40 +0800

    chore: update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,11 +3,15 @@ import configurePackage from './support/rollup/configure-package';
 import core from './packages/core/package.json';
 import rendererReact from './packages/renderer-react/package.json';
 import richTextEditorPlugin from './packages/plugin-rich-text-editor/package.json';
+import themeSelectorPlugin from './packages/plugin-theme-selector/package.json';
+import jsonSerializerPlugin from  './packages/plugin-json-serializer/package.json';
 
 const configs = [
   ...configurePackage(core),
   ...configurePackage(rendererReact),
-  ...configurePackage(richTextEditorPlugin)
+  ...configurePackage(richTextEditorPlugin),
+  ...configurePackage(themeSelectorPlugin),
+  ...configurePackage(jsonSerializerPlugin),
 ];
 
 export default configs;
