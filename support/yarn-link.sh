cb6fddac9a980fb97c6ed6c4543d1711d3475e50
awehook <awehook@163.com>
2019-11-18 19:10:09 +0800

    chore: add yarn link command

---
+++ support/yarn-link.sh
@@ -0,0 +1,16 @@
+ cd '../packages'
+ for dir in `ls .`
+ do
+   if [ -d $dir ]
+   then
+     cd $dir
+     if [ $1 == "link" ]
+     then
+      yarn link
+     elif [ $1 == "unlink" ]
+     then
+      yarn unlink
+      fi
+     cd ..
+   fi
+done
