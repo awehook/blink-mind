758b0f908b9e389124b91460e2cd9243fab390d7
awehook <awehook@163.com>
2019-11-19 16:52:34 +0800

    fix: fix css priority bug

--- stories/undo-redo-demo/index.stories.tsx
+++ stories/undo-redo-demo/index.stories.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import { storiesOf } from '@storybook/react';
 import { BaseDemo } from '../common/base-demo';
 import { Diagram, iconClassName } from '@blink-mind/renderer-react';
-import { ThemeSelectorPlugin } from '@blink-mind/plugin-theme-selector';
 import styled from 'styled-components';
 
 const ToolBar = styled.div`
--- stories/utils/index.ts
+++ stories/utils/index.ts
@@ -37,7 +37,7 @@ export function generateSimpleModel() {
       {
         key: 'sub2_1',
         parentKey: 'sub2',
-        blocks: [{ type: 'CONTENT', data: 'SubTopic2' }],
+        blocks: [{ type: 'CONTENT', data: 'SubTopic' }],
         collapse: false
       },
       {
