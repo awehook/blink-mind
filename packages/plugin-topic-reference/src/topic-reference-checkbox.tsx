7a6724848ddc2e60b131070b6e598e1e9dd3519f
awehook <awehook@163.com>
2020-01-01 13:40:38 +0800

    refactor: 找到useState报错原因，重新用回之前的代码

--- packages/plugin-topic-reference/src/topic-reference-checkbox.tsx
+++ packages/plugin-topic-reference/src/topic-reference-checkbox.tsx
@@ -2,56 +2,23 @@ import { KeyType } from '@blink-mind/core';
 import { BaseProps } from '@blink-mind/renderer-react';
 import { Checkbox } from '@blueprintjs/core';
 import * as React from 'react';
-// TODO useState 在vscode 运行报错 https://reactjs.org/docs/error-decoder.html/?invariant=321，暂时不知道如何解决
-// import { useState } from 'react';
-// export function TopicReferenceCheckbox(props) {
-//   const { topicKey, selectedTopicKeys } = props;
-//   const a = selectedTopicKeys.has(topicKey);
-//   const [checked, setChecked] = useState(a);
-//   const checkboxProps = {
-//     checked,
-//     onChange: () => {
-//       if (selectedTopicKeys.has(topicKey)) {
-//         selectedTopicKeys.delete(topicKey);
-//       } else {
-//         selectedTopicKeys.add(topicKey);
-//       }
-//       setChecked(!checked);
-//     }
-//   };
-//   return <Checkbox {...checkboxProps}></Checkbox>;
-// }
-
-interface Props extends BaseProps {
-  selectedTopicKeys: Set<KeyType>;
-}
-
-interface State {
-  checked: boolean;
-}
-
-export class TopicReferenceCheckbox extends React.Component<Props, State> {
-  constructor(props) {
-    super(props);
-    const { topicKey, selectedTopicKeys } = props;
-    this.state = {
-      checked: selectedTopicKeys.has(topicKey)
-    };
-  }
-  render() {
-    const { selectedTopicKeys, topicKey } = this.props;
-
-    const checkboxProps = {
-      checked: this.state.checked,
-      onChange: () => {
-        if (selectedTopicKeys.has(topicKey)) {
-          selectedTopicKeys.delete(topicKey);
-        } else {
-          selectedTopicKeys.add(topicKey);
-        }
-        this.setState({ checked: !this.state.checked });
+// useState 在vscode 运行报错 https://reactjs.org/docs/error-decoder.html/?invariant=321，
+// 问题原因，在yarn link package的时候，webpack 会打包进去多个 react-dom.js
+import { useState } from 'react';
+export function TopicReferenceCheckbox(props) {
+  const { topicKey, selectedTopicKeys } = props;
+  const a = selectedTopicKeys.has(topicKey);
+  const [checked, setChecked] = useState(a);
+  const checkboxProps = {
+    checked,
+    onChange: () => {
+      if (selectedTopicKeys.has(topicKey)) {
+        selectedTopicKeys.delete(topicKey);
+      } else {
+        selectedTopicKeys.add(topicKey);
       }
-    };
-    return <Checkbox {...checkboxProps}></Checkbox>;
-  }
+      setChecked(!checked);
+    }
+  };
+  return <Checkbox {...checkboxProps}></Checkbox>;
 }
